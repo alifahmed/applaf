71f2f8a49f9792c2fa681ef7c4fac854a4b1b02e
Alif Ahmed <alifahmed@virginia.edu>
2020-10-31 02:14:48 -0400

    Fixed persistent mode initialize. Previously going through the whole map

--- llvm_mode/afl-llvm-rt.o.c
+++ llvm_mode/afl-llvm-rt.o.c
@@ -239,7 +239,7 @@ int __afl_persistent_loop(unsigned int max_cnt) {
 
     if (is_persistent) {
 
-      memset(__afl_area_ptr, 0, MAP_SIZE);
+      memset(__afl_area_ptr, 0, __afl_idx_ptr[0]);
       __afl_area_ptr[0] = 1;
       __afl_prev_loc = 0;
 
